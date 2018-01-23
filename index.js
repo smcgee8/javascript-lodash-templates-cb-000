5f4b138776df74623e96a23e7475d35126d3547b
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-23 03:19:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ function postComment() {
   var commenter = document.getElementById("commenterName").value;
   var comment = document.getElementById("commentText").value;
 
-  var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
+  //var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
 
+  var commentTemplate = document.getElementById("comment-template").innerHTML;
+  
   var templateFn = _.template(commentTemplate);
 
   var commentsDiv = document.getElementById("comments");
